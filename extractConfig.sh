3b82b7c4ed8bc16d768ee588b68280677cd2aea7
Nick Patterson <nkpatterson@Nicks-iMac.lan>
2019-03-08 12:43:53 -0700

    Updated to only point to a single API instead of all APIs in an APIM instance

--- extractConfig.sh
+++ extractConfig.sh
@@ -1,5 +1,5 @@
 dir=$(pwd)
 pushd $APIM_TEMPLATE_TOOL_PATH
-dotnet run extract --name $APIM_DEV_NAME --resourceGroup $APIM_DEV_RG --fileFolder $dir
+dotnet run extract --name $APIM_DEV_NAME --resourceGroup $APIM_DEV_RG --fileFolder $dir --apiName $API_NAME
 popd
-mv "$APIM_DEV_NAME-apis-template.json" "$APIM_PROD_NAME-apis-template.json"
+mv "$APIM_DEV_NAME-$API_NAME-template.json" "$APIM_PROD_NAME-$API_NAME-template.json"
